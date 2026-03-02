3177a7d08e22380cfd63d6d3591fdacf1f100ec4
peter34512800 <peter34512800@gmail.com>
2026-03-02 17:25:04 +1030

    Added ip address connection for client

--- Client/main.cpp
+++ Client/main.cpp
@@ -41,7 +41,8 @@ int main(int argc, char **argv)
     memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(port);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    // serverAddress.sin_addr.s_addr = INADDR_ANY;
+    memcpy(&serverAddress.sin_addr.s_addr, server->h_addr, server->h_length);
 
     // TODO: connect
     int connectVal = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
